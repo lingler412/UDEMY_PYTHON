34664246e43a7ac56c0e89b36554fe20ae050a3f
Rob Lingle <roblingle2@gmail.com>
2020-03-08 08:54:35 -0400

    wanted to see if adding an int to an ont would print

--- input_int_exercise.py
+++ input_int_exercise.py
@@ -2,3 +2,4 @@ user_int = int(input("Please enter a whole number:  ")) # taking input and chang
 user_int += 10 #ading 10 to the user input, still an int
 print("I have added 10 to your number making it " + str(user_int)) # temp changing to a srting to concatenate and print
 print(type(user_int)) # showing that it is still an int
+print(user_int + 10)
