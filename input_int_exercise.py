f2309d472cadbea2d1f2182e9fc719b3499ad950
Rob Lingle <roblingle2@gmail.com>
2020-03-07 21:12:12 -0500

    adding input int exercise

---
+++ input_int_exercise.py
@@ -0,0 +1,4 @@
+user_int = int(input("Please enter a whole number:  ")) # taking input and changing from string to int
+user_int += 10 #ading 10 to the user input, still an int
+print("I have added 10 to your number making it " + str(user_int)) # temp changing to a srting to concatenate and print
+print(type(user_int)) # showing that it is still an int
