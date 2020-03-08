7399c901e9f65334fbdff69c60bbf31509d96405
Rob Lingle <roblingle2@gmail.com>
2020-03-07 19:58:09 -0500

    added my comments to strings exercise

--- first_string_exercise.py
+++ first_string_exercise.py
@@ -1,6 +1,6 @@
 famous_moto = "Just do it!"
-print(famous_moto[10])
-print(famous_moto[5:7])
-print(famous_moto[8:11])
-print(famous_moto[:4])
-print("Don't " + famous_moto[5:])
+print(famous_moto[10]) # prints !
+print(famous_moto[5:7]) # prints do
+print(famous_moto[8:11]) # prints it!
+print(famous_moto[:4]) # prints Just
+print("Don't " + famous_moto[5:]) # prints Don't do it!
