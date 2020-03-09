d1ff7d9154d4a6225ea8579ad21c08b2e8b74120
Rob Lingle <roblingle2@gmail.com>
2020-03-08 22:26:41 -0400

    gas miliage app change

--- gas_mileage.py
+++ gas_mileage.py
@@ -1,13 +1,13 @@
-from random import randint
+from random import randint # importing only what function I need (fucntion import)
 
 
-gal_per_tank = randint(10,25)
-miles_per_tank = randint(200,400)
+gal_per_tank = randint(10,25) # Randomly generting the gallons per tank
+miles_per_tank = randint(200,400) # Randomly generatig the miles dirven per full tank
 
 
-def calc_mpg(miles, gallons):
+def calc_mpg(miles, gallons): # chose to define a function...might be overkill :-)
     return miles // gallons
 
 
-mpg = calc_mpg(miles_per_tank, gal_per_tank)
-print(mpg)
+mpg = calc_mpg(miles_per_tank, gal_per_tank) # Passing my arguments
+print(mpg) 
