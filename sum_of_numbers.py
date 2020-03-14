cc3acaef232994f62512d3575a44658d8a542ea2
Rob Lingle <roblingle2@gmail.com>
2020-03-14 12:00:06 -0400

    updated sum of num

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -2,11 +2,12 @@ pos_int = int(input("Give me a positive whole number: ")) # get the input for th
 
 print("Thanks! I will now iteratatively add to " + str(pos_int)) # state the number I plan to iterate on
 
-sum_of_my_num = pos_int # creating a variabl to hold my sum as i iterate, and set it to my initial input from pos_int
+sum_of_my_num = 0 # creating a variabl to hold my sum as i iterate, and set it to my initial input from pos_int
 
-while pos_int >= 1: # loop to iteratively add up decrements of my original input
-    pos_int -= 1 # decrement my orgiinal input by 1
+while pos_int > 0: # loop to iteratively add up decrements of my original input
     sum_of_my_num += pos_int # ietratively sum my int as it is decremeted each loop
+    pos_int -= 1 # decrement my orgiinal input by 1
+    
        
 
 print("Your sum is " + str(sum_of_my_num)) # print my sum
