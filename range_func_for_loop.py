232a9d0bac8a3a7acf5c4d7129a6169432338ae4
Rob Lingle <roblingle2@gmail.com>
2020-03-15 10:44:34 -0400

    updated the for loop file

--- range_func_for_loop.py
+++ range_func_for_loop.py
@@ -4,12 +4,12 @@ range_of_nums = range(1,51) # generate a set of numbers between 1 and 50
 for num in range_of_nums:
     if num % 3 == 0 and num % 5 == 0: # is the num divisiable by 3 & 5?
         print("FizzBuzz")
-    elif num % 3 == 0 and num % 5 != 0: # is the num ONLY divisible by 3?
+    elif num % 3 == 0: # is the num ONLY divisible by 3?
         print("Fizz")
-    elif num % 3 != 0 and num % 5 == 0: # is the num ONLY divisible by 5?
+    elif num % 5 == 0 : # is the num ONLY divisible by 5?
         print("Buzz")
     else:
-        print(num) # if none of the above, just print the num
+        print(num) # if none of the above, just print the number
     
     
     
