7c119ace9cfc4aba1c439e6a50488d19c71705cf
Rob Lingle <roblingle2@gmail.com>
2020-03-24 20:19:34 -0400

    updated word counter to use a for loop and if statement to remove special characters

--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,13 @@
 user_str = input("Give me something to work with: ")
-indiv_words = user_str.strip("!.*").replace("!", "").replace("*", "").split() # sucks you can only replce one character at a time rather than a list
-num_of_words = len(indiv_words)
-print("Your " + str(num_of_words) + " words are " + str(indiv_words))
+# commenting out because a for loop is cleaner and accounts for all specoial characters:  indiv_words = user_str.strip("!.*").replace("!", "").replace("*", "").split() # sucks you can only replce one character at a time rather than a list
+
+no_special = ""
+
+for char in user_str:
+    if char.isalpha() or char.isspace() or char.isalnum():
+        no_special += char
+        
+
+num_of_words = len(no_special.split())
+
+print("Your " + str(num_of_words) + " words are " + str(no_special.split()))
